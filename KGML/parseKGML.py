1a5a0ed67cf1e5011bf198887390656bf35e7461
Pierre Jacquet <piejacquet@emi.u-bordeaux1.fr>
2019-01-01 12:10:20 +0100

    push du mating

--- KGML/parseKGML.py
+++ KGML/parseKGML.py
@@ -7,29 +7,84 @@ from os.path import isfile, join
 
 KGMLs = [f for f in listdir("./kgml_files/")
          if isfile(join("./kgml_files/", f))]
-print (KGMLs)
+#print (KGMLs)
 
 
 filename = open("./MAPLink.json", "w")
-filename.write("{\n")
+#filename.write('{\n"elements":{\n')
+filename.write('{')
+
+nodestring='"nodes":[\n'
+edgestring='"edges":[\n'
+assignId=0
+
 for files in KGMLs:
     path = "kgml_files/" + files
     Maps = KGML_parser.read(open(path, 'r'))
     name = Maps.name
-    filename.write('\t"' + name + '":{\n\t\t"connected_to": [\n')
+    nodestring+='{"data":{ "id":"' + name + '", "label":"'+name+'"}},'
+    #edgestring+='{"data":{ "id":"' + name + '", "label":"'+name+'"}},'
     lenMapmap = len(Maps.maps)
     for i in range(lenMapmap):
         entry = str(Maps.maps[i].name)
-        if i != lenMapmap - 1:
-            filename.write('\t\t\t\t"' + entry + '",\n')
-        else:
-            filename.write('\t\t\t\t"' + entry + '"\n')
-    filename.write("\t\t\t]\n\t},\n")
-    #print (element.name)
+        if entry != name:
+            nodestring+='{"data":{ "id":"' + entry + '", "label":"'+entry+'"}},'
+            edgestring+='{"data":{ "id":"' + str(assignId) + '", "source":"'+name+'","target":"'+entry+'"}},'
+            assignId=assignId+1
 
-filename.write("\n}")
+filename.write(nodestring)
+filename.write("],")
+filename.write(edgestring)
+filename.write("]")
+filename.write("}")
 filename.close()
 
+
+
+## for cytoscape desktop
+filename = open("./MAPLinkBureauversion.json", "w")
+#filename.write('{\n"elements":{\n')
+filename.write('''{  "format_version" : "1.0",
+  "generated_by" : "cytoscape-3.4.0",
+  "target_cytoscapejs_version" : "~2.1",
+  "data" : {
+    "shared_name" : "Network",
+    "name" : "Network",
+    "SUID" : 7430,
+    "shared_name" : "Network",
+    "__Annotations" : [ ],
+    "selected" : true
+  },
+  "elements" : {''')
+
+nodestring='"nodes":[\n'
+edgestring='"edges":[\n'
+assignId=0
+
+for files in KGMLs:
+    path = "kgml_files/" + files
+    Maps = KGML_parser.read(open(path, 'r'))
+    name = Maps.name
+    description=Maps.title
+    nodestring+='{"data":{ "id":"' + name + '", "name":"'+description+'"}},'
+    #edgestring+='{"data":{ "id":"' + name + '", "label":"'+name+'"}},'
+    lenMapmap = len(Maps.maps)
+    for i in range(lenMapmap):
+        entry = str(Maps.maps[i].name)
+        if entry != name and "mmu" in entry:
+            #nodestring+='{"data":{ "id":"' + entry + '", "name":"'+entry+'"}},'
+            edgestring+='{"data":{ "id":"' + str(assignId) + '", "source":"'+name+'","target":"'+entry+'"}},'
+            assignId=assignId+1
+
+filename.write(nodestring)
+filename.write("],")
+filename.write(edgestring)
+filename.write("]")
+filename.write("}}")
+filename.close()
+
+
+#sif lite version
 
 filename = open("./MAPLink.sif", "w")
 for files in KGMLs:
