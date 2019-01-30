dfb43e2009b15dd8cb240f41295697ca5474a787
Unknown <peepanjacquet@gmail.com>
2019-01-30 14:12:02 +0100

    push zboui

--- networkoverview.js
+++ networkoverview.js
@@ -1,37 +1,117 @@
-var graphsource="path:mmu01100";
-var graphtarget="path:mmu04010";
-var listselected = ["path:mmu05218", "path:mmu04115", "path:mmu04916", "path:mmu04010"];
-
-function filterGraph(cytoscapegraph) {
-  cytoscapegraph.nodes().forEach(function (ele) {
-    if (!listselected.includes(ele.data().id)) {
-      //cytoscapegraph.remove(ele);
-    }
-  });
-  return cytoscapegraph;
+function getUrlParams(search) {
+  let hashes = search.slice(search.indexOf("?") + 1).split("&");
+  return hashes.reduce((params, hash) => {
+    let [key, val] = hash.split("=");
+    return Object.assign(params, { [key]: decodeURIComponent(val) });
+  }, {});
 }
 
 
-$.getJSON("KGML/result/MAPLinkJS.json", function (data) {
+function shortestPaths(graph, directed_boolean) {
+  path = [];
+
+  var aStar = graph
+    .elements()
+    .aStar({
+      root: graph.$id(graphsource),
+      goal: graph.$id(graphtarget),
+      directed: directed_boolean
+    });
+  if (aStar["path"] === undefined){
+    $("body").append("<p style='position: absolute; top:49vh; width:100vw; text-align:center'>There is no path between "+graphsource+ " and "+graphtarget+".<br>Try to switch the graph mode (directed/undirected) or change one pathway.</p>");
+  }
+  else{
+    path.push(aStar.path.jsons());
+    for (item in aStar.path.jsons()) {
+      object = aStar.path.jsons()[item];
+      //console.log(object);
+  
+      if (object.group == "edges") {
+        var removed = graph.remove(graph.$id(object.data.id));
+        shortestPaths_recursive(graph, path, aStar.distance, directed_boolean);
+        removed.restore();
+      }
+    }
+    console.log(path);
+  
+    var collection = [].concat.apply([], path);
+    return collection;
+  }
+}
+
+function shortestPaths_recursive(graph, path, maxlength, directed_boolean) {
+  var aStar = graph
+    .elements()
+    .aStar({
+      root: graph.$id(graphsource),
+      goal: graph.$id(graphtarget),
+      directed: directed_boolean
+    });
+  if (aStar.distance <= maxlength) {
+    path.push(aStar.path.jsons());
+    for (item in aStar.path.jsons()) {
+      object = aStar.path.jsons()[item];
+      if (object.group == "edges") {
+        var removed = graph.remove(graph.$id(object.data.id));
+        shortestPaths_recursive(graph, path, maxlength, directed_boolean);
+        removed.restore();
+      }
+    }
+  }
+
+  //return path;
+}
+
+console.log(getUrlParams(window.location.search));
+getUrlParams(window.location.search);
+
+var graphsource = getUrlParams(window.location.search).path1;
+var graphtarget = getUrlParams(window.location.search).path2;
+var directed_boolean = JSON.parse(
+  getUrlParams(window.location.search).directed
+);
+if (directed_boolean == true) {
+  $(".button6").text("Switch to undirected mode");
+
+  $(".button6").click(function() {
+    location.href = location.href.replace("true", "false");
+  });
+} else {
+  $(".button6").text("Switch to directed mode");
+  $(".button6").click(function() {
+    location.href = location.href.replace("false", "true");
+  });
+}
+
+$.getJSON("KGML/result/MAPLinkJS.json", function(data) {
   //precy is a the full network object that must be processed before graph rendering.
   var precy = cytoscape({
     headless: true,
-    elements: data,
+    elements: data
   });
 
-  var aStar = precy.elements().aStar({ root: precy.$id(graphsource), goal: precy.$id(graphtarget), directed: true });
-  aStar.path.select();
-  console.log(aStar.path);
-  console.log(aStar.distance);
+  precy.edges().forEach(function(ele) {
+    codirectedEdges = precy.$(ele).codirectedEdges();
+    if (codirectedEdges.length > 1) {
+      codirectedEdges.remove(codirectedEdges[0]); //remove one edge to the collection to keep it safe from deletion after.
+      precy.remove(precy.$(ele).codirectedEdges());
+    }
+  });
 
-  filterGraph(precy)
+  if (graphtarget == "") {
+    //if graphtarget is undefined show just the unique pathway
+    elements_data = precy.filter(precy.$id(graphsource));
+    elements_data = elements_data.jsons();
+  } else {
+    //else show the shortest path between graph_source and graph_target
+    elements_data = shortestPaths(precy, directed_boolean);
+  }
 
-  console.log(precy);
   var cy = cytoscape({
     container: document.getElementById("cy"), // container to render in
 
-    elements: aStar.path.jsons(),
-
+    //elements: shortestPaths(precy, directed_boolean),
+    elements: elements_data,
     layout: {
       name: "cola",
       animate: true,
@@ -42,87 +122,92 @@ $.getJSON("KGML/result/MAPLinkJS.json", function (data) {
       nodeDimensionsIncludeLabels: true,
       avoidOverlap: true,
       handleDisconnected: false,
-      nodeSpacing: function (node) {
+      nodeSpacing: function(node) {
         return 0;
       },
-      // flow: {
-      //   axis: "y"
-      // },
+      flow: {
+        axis: "y"
+      },
       unconstrIter: 1000,
       userConstIter: 1000,
-      infinite: false
-
+      infinite: true
     },
 
-    style: [{
-      "selector": "core",
-      "style": {
-        "selection-box-color": "#AAD8FF",
-        "selection-box-border-color": "#8BB0D0",
-        "selection-box-opacity": "0.5"
+    style: [
+      {
+        selector: "core",
+        style: {
+          "selection-box-color": "#AAD8FF",
+          "selection-box-border-color": "#8BB0D0",
+          "selection-box-opacity": "0.5"
+        }
+      },
+      {
+        selector: "node",
+        style: {
+          content: "data(name)",
+          "font-size": "12px",
+          "text-valign": "center",
+          "text-halign": "center",
+          "background-color": "#555",
+          "text-outline-color": "#555",
+          "text-outline-width": "2px",
+          color: "#fff",
+          //"overlay-padding": "6px",
+          "z-index": "10"
+        }
+      },
+      {
+        selector: "node:selected",
+        style: {
+          "border-width": "6px",
+          "border-color": "#AAD8FF",
+          "border-opacity": "0.5",
+          "background-color": "#77828C",
+          "text-outline-color": "#77828C"
+        }
+      },
+      {
+        selector: "edge",
+        style: {
+          "curve-style": "bezier",
+          "target-arrow-shape": "triangle",
+          opacity: "0.4",
+          "line-color": "#bbb",
+          width: "mapData(weight, 0, 1, 1, 8)"
+          //"overlay-padding": "3px"
+        }
+      },
+      {
+        selector: "node.unhighlighted",
+        style: {
+          opacity: "0.2"
+        }
+      },
+      {
+        selector: "edge.unhighlighted",
+        style: {
+          opacity: "0.05"
+        }
+      },
+      {
+        selector: ".highlighted",
+        style: {
+          "z-index": "999999"
+        }
+      },
+      {
+        selector: "node.highlighted",
+        style: {
+          "border-width": "6px",
+          "border-color": "#AAD8FF",
+          "border-opacity": "0.5",
+          "background-color": "#394855",
+          "text-outline-color": "#394855"
+        }
       }
-    }, {
-      "selector": "node",
-      "style": {
-        "content": "data(name)",
-        "font-size": "12px",
-        "text-valign": "center",
-        "text-halign": "center",
-        "background-color": "#555",
-        "text-outline-color": "#555",
-        "text-outline-width": "2px",
-        "color": "#fff",
-        //"overlay-padding": "6px",
-        "z-index": "10"
-      }
-    }, {
-      "selector": "node:selected",
-      "style": {
-        "border-width": "6px",
-        "border-color": "#AAD8FF",
-        "border-opacity": "0.5",
-        "background-color": "#77828C",
-        "text-outline-color": "#77828C"
-      }
-    }, {
-      "selector": "edge",
-      "style": {
-        "curve-style": "bezier",
-        "target-arrow-shape": "triangle",
-        "opacity": "0.4",
-        "line-color": "#bbb",
-        "width": "mapData(weight, 0, 1, 1, 8)"
-        //"overlay-padding": "3px"
-      }
-    }, {
-      "selector": "node.unhighlighted",
-      "style": {
-        "opacity": "0.2"
-      }
-    }, {
-      "selector": "edge.unhighlighted",
-      "style": {
-        "opacity": "0.05"
-      }
-    }, {
-      "selector": ".highlighted",
-      "style": {
-        "z-index": "999999"
-      }
-    }, {
-      "selector": "node.highlighted",
-      "style": {
-        "border-width": "6px",
-        "border-color": "#AAD8FF",
-        "border-opacity": "0.5",
-        "background-color": "#394855",
-        "text-outline-color": "#394855"
-      }
-    }]
+    ]
   });
 
   cy.center();
-
-
-
 });
---
+++ search.js
@@ -0,0 +1,179 @@
+function delay(callback, ms) {
+  var timer = 0;
+  return function() {
+    var context = this,
+      args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function() {
+      callback.apply(context, args);
+    }, ms || 0);
+  };
+}
+
+function findAndSetPathway(dict, item, id_selector) {
+  $(id_selector).html("");
+  for (path_data in dict.nodes) {
+    pathway = dict.nodes[path_data];
+    console.log(pathway["data"]["compounds"]);
+    if (
+      pathway["data"]["compounds"].indexOf(item) > -1 ||
+      pathway["data"]["gene products"].indexOf(item) > -1
+    ) {
+      $(id_selector).append(
+        "<option value=" +
+          pathway["data"]["id"] +
+          ">" +
+          pathway["data"]["name"] +
+          "</option>"
+      );
+    }
+  }
+}
+
+$.ajaxSetup({ async: false });
+
+var MAPLink = $.getJSON("KGML/result/MAPLinkJS.json", function(MAPLink) {
+  console.log(MAPLink); // this will show the info it in firebug console
+  return MAPLink;
+});
+
+var compound = $.getJSON("KGML/result/compound_parsed.json", function(
+  compound
+) {
+  return compound;
+});
+
+var gene = $.getJSON("KGML/result/mmugenes_parsed.json", function(gene) {
+  return gene;
+});
+
+MAPLink = JSON.parse(MAPLink.responseText);
+compound = JSON.parse(compound.responseText);
+gene = JSON.parse(gene.responseText);
+comp_gene = compound.Compounds.concat(gene.Gene);
+
+var options = {
+  shouldSort: true,
+  threshold: 0.3,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: ["id", "name"]
+};
+
+$("#searchbar").keyup(
+  delay(function(e) {
+   if (e.keyCode != 13) {
+    var val = $("#searchbar").val();
+    if (val != " ") {
+      var fuse = new Fuse(comp_gene, options); // "list" is the item array
+      var result = fuse.search(val);
+      $("#result").html("");
+      for (numObject in result) {
+        object = result[numObject];
+        object_id = object["id"];
+        object_names = object["name"].join(" , ");
+        if (object_id.indexOf("cpd:") >= 0) {
+          $("#result").append(
+            "<div class='result-item' id='" +
+              object["id"] +
+              "'><p>KEGG Compounds ID: " +
+              object["id"] +
+              "</p><p><b>Possible names:</b> " +
+              object_names +
+              "</p></div>"
+          );
+        } else {
+          $("#result").append(
+            "<div class='result-item'  id='" +
+              object["id"] +
+              "'><p>KEGG gene product ID: " +
+              object["id"] +
+              "</p><p><b>Possible names:</b> " +
+              object_names +
+              "</p></div>"
+          );
+        }
+      }
+   }
+    }
+    $(".result-item").click(function() {
+      var text = $(this).attr("id");
+      $("#searchbar").val(text);
+      $("#result").html("");
+      findAndSetPathway(MAPLink, text, "#searchbar-pathway");
+      // do something with the text
+    });
+  }, 1500)
+);
+
+$("#searchbar").on('keyup',function (e) {
+   if (e.keyCode == 13) {
+      var text = $("#searchbar").val();
+      $("#result").html("");
+      findAndSetPathway(MAPLink, text, "#searchbar-pathway");
+   }});
+
+
+$("#searchbar2").on('keyup',function (e) {
+   if (e.keyCode == 13) {
+      var text = $("#searchbar2").val();
+      $("#result2").html("");
+      findAndSetPathway(MAPLink, text, "#searchbar2-pathway");
+   }});
+
+
+$("#searchbar2").keyup(
+   delay(function(e) {
+      if (e.keyCode != 13) {
+     var val = $("#searchbar2").val();
+     if (val != " ") {
+       var fuse = new Fuse(comp_gene, options); // "list" is the item array
+       var result = fuse.search(val);
+       $("#result2").html("");
+       for (numObject in result) {
+         object = result[numObject];
+         object_id = object["id"];
+         object_names = object["name"].join(" , ");
+         if (object_id.indexOf("cpd:") >= 0) {
+           $("#result2").append(
+             "<div class='result-item2' id='" +
+               object["id"] +
+               "'><p>KEGG Compounds ID: " +
+               object["id"] +
+               "</p><p><b>Possible names:</b> " +
+               object_names +
+               "</p></div>"
+           );
+         } else {
+           $("#result2").append(
+             "<div class='result-item2'  id='" +
+               object["id"] +
+               "'><p>KEGG gene product ID: " +
+               object["id"] +
+               "</p><p><b>Possible names:</b> " +
+               object_names +
+               "</p></div>"
+           );
+         }
+       }
+      }
+     }
+     $(".result-item2").click(function() {
+       var text = $(this).attr("id");
+       $("#searchbar2").val(text);
+       $("#result2").html("");
+       findAndSetPathway(MAPLink, text, "#searchbar2-pathway");
+       // do something with the text
+     });
+   }, 1500)
+ );
+
+
+$(".button6").click(function(){
+   var path1=$('#searchbar-pathway').find(":selected").attr("value");
+   var path2=$('#searchbar2-pathway').find(":selected").attr("value");
+   var href="./graph.html?path1="+path1+"&path2="+path2+"&directed=true";
+   window.location.href = href;
+})
